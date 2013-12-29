84bb2dd40f43d7bd63766c7ee8e2b722a3397107
Zachariah Reitano <zreitano@gmail.com>
2013-12-29 14:03:07 -0500

    Update play.rb

--- __george_bixby/midterm/play.rb
+++ __george_bixby/midterm/play.rb
@@ -9,9 +9,12 @@ require './times_headline'
 while @year <= 1900 || @year >= 2013 do
   puts "Please enter a date between 1900 and 2013"
   year = gets
+  #gets.chomp (in case the user input something with a space)
   @year = year.to_i
 end
 
+#Why is @year an instance variable. If you are simply using year as a variable to be able to pass it into
+# TimesHealine.new(year) it does not need to be an instance variable. 
 
 t = TimesHeadline.new(@year)
 w = WordCounter.new(t.headlines)
